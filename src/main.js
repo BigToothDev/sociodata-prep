a24dd822024a8bd02d0a7878c7e9f6464a20e6d1
Alex Lytvynenko <oleksandr.lit@gmail.com>
2025-07-03 15:12:55 +0300

    Add menu base

---
+++ src/main.js
@@ -0,0 +1,13 @@
+function onOpen() {
+    let ui = SpreadsheetApp.getUi();
+    ui.createMenu('SD-prep')
+        .addSubMenu(
+            ui.createMenu('Recode', 'recode_submenu')
+                .addItem('Dichotomous', 'recode_dich')
+                .addItem('Categorical', 'recode_cat')
+        )
+        .addSeparator()
+        .addItem('Add Response IDs', "resp_id")
+        .addItem('Track Dynamics', 'dynam')
+        .addToUi();
+};
