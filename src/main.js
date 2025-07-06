1cb883bd42b89cba08927749b0816708e5443a0f
Alex Lytvynenko <oleksandr.lit@gmail.com>
2025-07-06 22:02:13 +0300

    Modify main.js menu order

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 function onOpen() {
     let ui = SpreadsheetApp.getUi();
     ui.createMenu('SD-prep')
-        .addSubMenu(
-            ui.createMenu('Recode')
-                .addItem('Dichotomous', 'recode_dich')
-                .addItem('Categorical', 'recode_cat')
-        )
-        .addSeparator()
-        .addItem('Add Response IDs', "resp_id")
+        .addItem('Calculate sample', 'sample_rand')
+        .addItem('Add Response IDs', 'resp_id')
         .addItem('Track Dynamics', 'dynam')
+        .addSeparator()
+        .addSubMenu(
+            ui.createMenu('Transform multichoice')
+                .addItem('Dichotomous', 'trf_dich')
+                .addItem('Categorical', 'trf_cat')
+        )        
         .addToUi();
 };
