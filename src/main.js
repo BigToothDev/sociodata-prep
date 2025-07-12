06d0eda69e5b9896c99bf8d6cdebe477769d6b6f
Alex Lytvynenko <oleksandr.lit@gmail.com>
2025-07-12 21:29:11 +0300

    Update main.js with num_recode and pivot

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 function onOpen() {
     let ui = SpreadsheetApp.getUi();
     ui.createMenu('SD-prep')
-        .addItem('Calculate sample', 'sample_rand')
+        .addItem('Calculate Sample', 'sample_rand')
         .addItem('Add Response IDs', 'resp_id')
         .addItem('Track Dynamics', 'dynam')
         .addSeparator()
@@ -9,6 +9,9 @@ function onOpen() {
             ui.createMenu('Transform multichoice')
                 .addItem('Dichotomous', 'trf_dich')
                 .addItem('Categorical', 'trf_cat')
-        )        
+        )
+        .addSeparator()
+        .addItem('Recode Numerically', 'num_recode')
+        .addItem('Pivot', 'pivot')
         .addToUi();
 };
