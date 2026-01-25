660e5481954f71979ac613bbf0b0d00390e713e5
Alex Lytvynenko <oleksandr.lit@gmail.com>
2026-01-25 13:12:23 +0200

    Fix sampleRandom z-score at first iteration & cols reduction

--- src/functions/sampleRandom.ts
+++ src/functions/sampleRandom.ts
@@ -5,7 +5,7 @@ const STDEV: number = 0.5;
 const POS_CONF_LVLS: number[] = [0.80, 0.90, 0.95, 0.98, 0.99];
 const POS_zScoreS: number[] = [1.282, 1.645, 1.960, 2.326, 2.576];
 const TABLE_STRUCTURE: string[] = ['confidence level', 'margin of error', 'standard deviation', 'z-score', 'population', 'sample', 'target'];
-const DEFAULT_CALC_DATA: [string, number][] = [['confidence level', CONF_LVL], ['margin of error', MARGIN_ERROR], ['standard deviation', STDEV], ['z-score', POS_CONF_LVLS[2]]];
+const DEFAULT_CALC_DATA: [string, number][] = [['confidence level', CONF_LVL], ['margin of error', MARGIN_ERROR], ['standard deviation', STDEV], ['z-score', POS_zScoreS[2]]];
 
 function sampleRandom(): void | GoogleAppsScript.Base.Button {
   const context = getSheetContext();
@@ -26,42 +26,47 @@ function sampleRandom(): void | GoogleAppsScript.Base.Button {
       if (button == ui.Button.OK) {
         const sampleSheet: GoogleAppsScript.Spreadsheet.Sheet = spreadsheet.insertSheet().setName(SAMPLE_SHEET_NAME);
         sampleSheet.getRange(1, 1, DEFAULT_CALC_DATA.length, DEFAULT_CALC_DATA[0].length).setValues(DEFAULT_CALC_DATA);
-        let sample: number = (Math.pow(POS_zScoreS[2], 2) * population * Math.pow(STDEV, 2)) / ((Math.pow(MARGIN_ERROR, 2) * population) + (Math.pow(POS_zScoreS[2], 2) * Math.pow(STDEV, 2)));
-        let newCalcData: [string, number][] = [['population', population], ['sample', sample], ['target', sample / population]];
+        const sample: number = (Math.pow(POS_zScoreS[2], 2) * population * Math.pow(STDEV, 2)) / ((Math.pow(MARGIN_ERROR, 2) * population) + (Math.pow(POS_zScoreS[2], 2) * Math.pow(STDEV, 2)));
+        const newCalcData: [string, number][] = [['population', population], ['sample', sample], ['target', sample / population]];
         sampleSheet.getRange(5, 1, newCalcData.length, newCalcData[0].length).setValues(newCalcData);
+        sampleSheet.deleteRows(8, sampleSheet.getMaxRows() - 7);
+        sampleSheet.deleteColumns(3, sampleSheet.getMaxColumns() - 2);
       } else {
-        ui.alert('User aborted request')
+        ui.alert('User aborted request');
       }
     } else {
-      let firstColVals = toCreateSheet.getRange(1, 1, toCreateSheet.getLastRow()).getValues() as string[][];
-      let indCols: number[] = Array.from({ length: firstColVals.length }, (e, i) => i + 1);
-      let valueRowPairs: [string, number][] = firstColVals.map((row, i) => [row[0], indCols[i]]);
-      let matchRowVals: [string, number][] = [];
+      const firstColVals = toCreateSheet.getRange(1, 1, toCreateSheet.getLastRow()).getValues() as string[][];
+      const indCols: number[] = Array.from({ length: firstColVals.length }, (e, i) => i + 1);
+      const valueRowPairs: [string, number][] = firstColVals.map((row, i) => [row[0], indCols[i]]);
+      const matchRowVals: [string, number][] = [];
       for (let i = 0; i < valueRowPairs.length; i++) {
-        let curVal = String(valueRowPairs[i][0]).trim().toLowerCase();
+        const curVal = String(valueRowPairs[i][0]).trim().toLowerCase();
         for (let u = 0; u < TABLE_STRUCTURE.length; u++) {
-          let searchVal = String(TABLE_STRUCTURE[u]).trim().toLowerCase();
+          const searchVal = String(TABLE_STRUCTURE[u]).trim().toLowerCase();
           if (curVal === searchVal) {
             matchRowVals.push(valueRowPairs[i]);
             break;
           }
         }
       }
-      let userConfLevel: number = toCreateSheet.getRange(matchRowVals[0][1], 2).getValue();
-      let userMarginError: number = toCreateSheet.getRange(matchRowVals[1][1], 2).getValue();
-      let userSTDEV: number = toCreateSheet.getRange(matchRowVals[2][1], 2).getValue();
-      let userPopulation: number = toCreateSheet.getRange(matchRowVals[4][1], 2).getValue();
+      const userConfLevel: number = toCreateSheet.getRange(matchRowVals[0][1], 2).getValue();
+      const userMarginError: number = toCreateSheet.getRange(matchRowVals[1][1], 2).getValue();
+      const userSTDEV: number = toCreateSheet.getRange(matchRowVals[2][1], 2).getValue();
+      const userPopulation: number = toCreateSheet.getRange(matchRowVals[4][1], 2).getValue();
       if (isNaN(Number(userConfLevel)) || isNaN(Number(userMarginError)) || isNaN(Number(userSTDEV)) || isNaN(Number(userPopulation))) return ui.alert('Some parameter is not a number');
       if (!POS_CONF_LVLS.includes(userConfLevel)) return ui.alert('Inappropriate confidence level (proper 0.80, 0.90, 0.95, 0.98, 0.99)');
       if (userMarginError < 0.01 || userMarginError > 0.05) return ui.alert('Inappropriate margin of error (proper [0.01; 0.05])');
       if (userSTDEV != 0.5) return ui.alert('Standard deviation cannot be other than 5');
-      let zIndex: number = POS_CONF_LVLS.indexOf(userConfLevel);
+      const zIndex: number = POS_CONF_LVLS.indexOf(userConfLevel);
       if (zIndex === -1) return ui.alert('Something went wrong with z-score defining');
-      let zScore: number = POS_zScoreS[zIndex];
-      let user_sample: number = (Math.pow(zScore, 2) * userPopulation * Math.pow(userSTDEV, 2)) / ((Math.pow(userMarginError, 2) * userPopulation) + (Math.pow(zScore, 2) * Math.pow(userSTDEV, 2)));
+      const zScore: number = POS_zScoreS[zIndex];
+      const userSample: number = (Math.pow(zScore, 2) * userPopulation * Math.pow(userSTDEV, 2)) / ((Math.pow(userMarginError, 2) * userPopulation) + (Math.pow(zScore, 2) * Math.pow(userSTDEV, 2)));
       toCreateSheet.getRange(matchRowVals[3][1], 2).setValue(zScore);
-      toCreateSheet.getRange(matchRowVals[5][1], 2).setValue(user_sample);
-      toCreateSheet.getRange(matchRowVals[6][1], 2).setValue(user_sample / userPopulation);
+      toCreateSheet.getRange(matchRowVals[5][1], 2).setValue(userSample);
+      toCreateSheet.getRange(matchRowVals[6][1], 2).setValue(userSample / userPopulation);
+      toCreateSheet.deleteRows(8, toCreateSheet.getMaxRows() - 7);
+      toCreateSheet.deleteColumns(3, toCreateSheet.getMaxColumns() - 2);
+      toCreateSheet.autoResizeColumns(1, 2);
     }
   } catch (error) {
     const message = error instanceof Error ? error.message : String(error);
